07e6bf8ec9400fdcbedf1c7cbe8056e4381d0559
Andreas Schenkel <Andreas Schenkel>
2022-11-22 10:34:55 +0100

    one line description

--- settings.php
+++ settings.php
@@ -15,6 +15,8 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+ * Settings for this block plugin.
+ *
  * @package    block_overviewmyrolesincourses
  * @copyright  Andreas Schenkel
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
